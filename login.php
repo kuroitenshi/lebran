6a15ec68cb7240aeaf666e15b54e9822e9cd41b2
kayelyn08 <kayelyn08@users.noreply.github.com>
2018-03-29 23:25:15 +0800

    admin page

--- login.php
+++ login.php
@@ -19,11 +19,12 @@ else { // User exists
         $_SESSION['last_name'] = $user['last_name'];
         $_SESSION['active'] = $user['active'];
         $_SESSION['id'] = $user['id'];
+        $_SESSION['type'] = $user['type'];
         
         // This is how we'll know the user is logged in
         $_SESSION['logged_in'] = true;
 
-        header("location: profile.php");
+        header("location: route.php");
     }
     else {
         $_SESSION['message'] = "You have entered wrong password, try again!";
