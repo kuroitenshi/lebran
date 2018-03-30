4d5279c81c4261c3bd6942f7b898eb76153ac552
Aldrich Gutierrez <AldrichPhilbert.Gutierrez@finastra.com>
2018-03-30 18:29:52 +0800

    Modified Landing Page Design

--- index.php
+++ index.php
@@ -6,8 +6,10 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Sign-Up/Login Form</title>
-  <?php include 'css/css.html'; ?>
+  <title> LeBran - Welcome! </title>
+  <?php include 'css/loadBootstrap.html'; ?>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <?php 
@@ -26,117 +28,151 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     }
 }
 ?>
-<body>
-  <div class="form">
-      
-      <ul class="tab-group">
-        <li class="tab"><a href="#signup">Sign Up</a></li>
-        <li class="tab active"><a href="#login">Log In</a></li>
-      </ul>
-      
-      <div class="tab-content">
+<body class="lebranHomeBody" >
 
-         <div id="login">   
-          <h1>Welcome Back!</h1>
-          
-          <form action="index.php" method="post" autocomplete="off">
-          
-            <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email" required autocomplete="off" name="email"/>
-          </div>
-          
-          <div class="field-wrap">
-            <label>
-              Password<span class="req">*</span>
-            </label>
-            <input type="password" required autocomplete="off" name="password"/>
-          </div>
-          
-          <p class="forgot"><a href="forgot.php">Forgot Password?</a></p>
-          
-          <button class="button button-block" name="login" />Log In</button>
-          
-          </form>
 
-        </div>
+  <div class="container lebranHomeContainer">
+
+    <div class="row">
+      
+      <div class="col-md-6 col-md-offset-3">
+
+        <div class = "panel panel-default fade in lebranHomePanel">
+          <div class="panel-heading"> <h1 style="text-align: center">LeBran (LOGO)</h1></div>
+          <div class = "panel-body">
+            <ul class="nav nav-tabs nav-justified">
+              <li class="active"><a data-toggle="tab" href="#loginScreen">LOGIN</span></a></li>
+              <li><a data-toggle="tab" href="#registerScreen">SIGN IN</a></li>
           
-        <div id="signup">   
-          <h1>Sign Up</h1>
-          
+            </ul>
+            <div class="tab-content">
+              <div id="loginScreen" class="tab-pane fade in active">
+                
+              <br>
+                <form action="index.php" method="post" autocomplete="off">
+                  <div class="form-group">
+                    
+                    <label>
+                      Email Address<span class="req">*</span>
+                    </label>
+                    <input type="email" class= "form-control" required autocomplete="off" name="email"/>
+                
+                  </div>
+                  <div class="form-group">
+                    <label>
+                      Password<span class="req">*</span>
+                    </label>
+                    <input type="password" class = "form-control" required autocomplete="off" name="password"/>
+                  
+                  </div>
+                      
+                  <p class="forgot"><a href="forgot.php">Forgot Password?</a></p>
+                  
+                  <button class="col-md-12 btn btn-success center-block" name="login" />Login</button>
+                  
+                </form>
+              </div>
+              <div id="registerScreen" class="tab-pane fade">
+          <br>
           <form action="index.php" method="post" autocomplete="off" enctype="multipart/form-data">
-          
-          <div class="top-row">
-            <div class="field-wrap">
-              <label>
-                First Name<span class="req">*</span>
-              </label>
-              <input type="text" required autocomplete="off" name='firstname' />
+          <h5>Profile Details</h5>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label>
+                  First Name<span class="req">*</span>
+                </label>
+                <input type="text" class= "form-control" required autocomplete="off" name='firstname' />
+              </div>
+
             </div>
-        
-            <div class="field-wrap">
-              <label>
-                Last Name<span class="req">*</span>
-              </label>
-              <input type="text"required autocomplete="off" name='lastname' />
+            <div class="col-md-6">
+              <div class="form-group">
+                <label>
+                  Last Name<span class="req">*</span>
+                </label>
+                <input type="text" class= "form-control" required autocomplete="off" name='lastname' />
+              </div>
             </div>
           </div>
 
-          <div class="field-wrap">
-            <label>
-              Email Address<span class="req">*</span>
-            </label>
-            <input type="email"required autocomplete="off" name='email' />
+          <div class="row">
+            <div class="col-md-7">
+              <div class="form-group">
+                <label>
+                  Email Address<span class="req">*</span>
+                </label>
+                <input type="email" class= "form-control" required autocomplete="off" name='email' />
+              </div>
+            </div>
+            <div class="col-md-5">
+              <div class="form-group">
+                <label>
+                  Password<span class="req">*</span>
+                </label>
+                <input type="password" class= "form-control" required autocomplete="off" name='password'/>
+              </div>
+            </div>
           </div>
           
-          <div class="field-wrap">
-            <label>
-              Set A Password<span class="req">*</span>
-            </label>
-            <input type="password" required autocomplete="off" name='password'/>
-          </div>
-
-          <div class="field-wrap">
+          <div class="form-group">
             <label for="address" >Address<span class="req">*</span></label>
-            <input type="text" required autocomplete="off" name="address"/>
+            <input type="text" class= "form-control" required autocomplete="off" name="address"/>
           </div>
 
-          <div class="field-wrap">
-            <h3>Birthday</h3>
-            <input type="date"  required name="birthday"/>
+          <div class="row">
+            <div class="col-md-5">
+              <div class="form-group">
+                <label for="birthday" >Birthday<span class="req">*</span></label>
+                <input type="date" class= "form-control" required name="birthday"/>
+              </div>
+            </div>
+            <div class="col-md-7">
+              <div class="form-group">
+                <label for="profilePhoto" >Profile Photo<span class="req">*</span></label>
+                <input type="file" class= "form-control" size="30" accept="image/*" name="profilePhoto" id = "profilePhoto"/>            
+              </div>
+            </div>
           </div>
+
+          <h5>BDO Account Details</h5>
           
-          <div class="field-wrap">
-            <h3>Upload Profile Photo</h3>
-            <input type="file" size="60" accept="image/*" name="profilePhoto" id = "profilePhoto"/>            
-          </div>
-
-          <div class="field-wrap">
-            <h3>BDO Account Details</h3>
-          </div>
-
-          <div class="field-wrap">
-            <label for="accountName">Account Name</label>
-            <input type="text" required name="accountName"/>
-          </div>
-
-          <div class="field-wrap">
+          <div class="form-group">
             <label for="accountNumber">Account Number</label>
-            <input type="text" required name="accountNumber"/>
+            <input type="text" class= "form-control" required name="accountNumber"/>
+          </div>
+
+          <div class="form-group">
+            <label for="accountName">Account Name</label>
+            <input type="text" class= "form-control" required name="accountName"/>
+          </div>
+            
+          <button type="submit" class="col-md-12 btn btn-success center-block" name="register" />Register</button>                 
+           
           </div>
           
-          <button type="submit" class="button button-block" name="register" />Register</button>
           
           </form>
+        </div>
 
-        </div>  
+          </div>
+   
+          <div class = "panel-footer">Copyright © 2018</div>
+          </div>
+
         
-      </div><!-- tab-content -->
+        
+        
+
+    </div>
+      </div>
       
-</div> <!-- /form -->
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    </div>
+    
+
+      
+  </div>
+
   <script>
     $("document").ready(function(){
 
